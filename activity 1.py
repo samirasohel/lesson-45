75061c8d2702de38bd62857f0e86f15798b92b93
samirasohel <samirasohel.othoi@gmail.com>
2024-12-14 12:25:49 +0600

    act 1

---
+++ activity 1.py
@@ -0,0 +1,10 @@
+#Write a program to create a class with name Student and perform the following tasks -
+#  Declare a variable grade 
+# Print a sentence inside the class 
+# Create an object of class student and see the output
+
+class student:
+    grade=10
+    print("I am student of grade",grade)
+
+st=student()
